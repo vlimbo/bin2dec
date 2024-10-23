c4a7063bec547a291f750d0ca7f7c6da40907d78
Vetle Limbodal <vwlimbo@proton.me>
2024-10-23 17:58:26 +0200

    Finish binary to decimal conversion calculator
    Created function to convert binary to decimal.

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const inputField = document.querySelector('.main__input');
 const warning = document.querySelector('.main__warning');
 const btn = document.querySelector('.main__btn');
+const convertBtn = document.querySelector('.main__convert-btn');
 
 // functions
 const convert = () => {
-    
+    const decimalArr = [];
+    let binPosition = inputField.value.length - 1;
+
+    for (let i = 0; i < inputField.value.length; i++) {
+        if (inputField.value[i] === '1') {
+            decimalArr.push(2**binPosition);
+        }
+        binPosition--;
+    }
+    return decimalArr.reduce((start, current) => start + current);
 }
 
 // eventlisteners
@@ -15,12 +25,16 @@ inputField.addEventListener('keydown', (event) => {
         inputField.disabled = true;
         btn.style.display = 'block';
     }
-})
+});
 
 btn.addEventListener('click', () => {
     warning.style.display = '';
     inputField.disabled = false;
     btn.style.display = '';
-})
+});
+
+convertBtn.addEventListener('click', () => {
+    console.log(convert());
+});
 
 
