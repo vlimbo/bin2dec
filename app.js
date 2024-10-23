e3b11820bcce772a0f9bd63c25590792a9464663
Vetle Limbodal <vwlimbo@proton.me>
2024-10-23 17:10:10 +0200

    Create logic that interupts user if type not a num
    If user types in anything but the values that are allowed, the user will
    get interrupted and have to change the values.

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello")
+// variables
+const inputField = document.querySelector('.main__input');
+const warning = document.querySelector('.main__warning');
+const btn = document.querySelector('.main__btn');
+
+// functions
+
+// eventlisteners
+inputField.addEventListener('keydown', (event) => {
+    if (event.key !== '1' && event.key !== '0' && event.key !== 'Delete' && event.key !== 'Backspace') {
+        warning.style.display = 'block';
+        inputField.disabled = true;
+        btn.style.display = 'block';
+    }
+})
+
+btn.addEventListener('click', () => {
+    warning.style.display = '';
+    inputField.disabled = false;
+    btn.style.display = '';
+})
+
+
